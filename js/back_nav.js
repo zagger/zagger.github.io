28975069a30d3d027a62ccc680d2429e608e1dee
r_ito <ir47839@gmail.com>
2014-12-31 11:04:41 +0900

    Initial commit

---
+++ js/back_nav.js
@@ -0,0 +1,86 @@
+$(function (){
+	var NavList = Backbone.Model.extend({
+		defaults: {
+			title: 'no title'
+		}
+	});
+	var NavLists = Backbone.Collection.extend({
+		model: NavList
+	});
+
+	var NavListView = Backbone.View.extend({
+		tagName: 'div',
+		className: 'nav-list',
+		template: _.template( $('#nav-template').html() ),
+		render: function() {
+			var template = this.template(this.model.toJSON());
+			this.$el.html(template);
+			return this;
+		}
+	});
+	var NavListsView = Backbone.View.extend({
+		tagName: 'nav',
+		render: function() {
+			this.collection.each(function(list) {
+				var navListView = new NavListView({model: list});
+				this.$el.append(navListView.render().el);
+			}, this);
+			return this;
+		}
+	});
+
+	var navLists = new NavLists([
+		{
+			title: 'Objective',
+			link: '#objective'
+		},
+		{
+			title: 'Activity',
+			link: '#activity'
+		},
+		{
+			title: 'Groups',
+			link: '#groups'
+		},
+		{
+			title: 'About',
+			link: '#about'
+		}
+	]);
+
+	var navListsView = new NavListsView({collection: navLists});
+
+	$('header').after(navListsView.render().el);
+
+	function p(v) {
+		console.log(v);
+	}
+
+	var Router = Backbone.Router.extend({
+		routes: {
+			'main': 'showMain',
+			'objective': 'showObjective',//route: method
+			'activity': 'showActivity',
+			'groups': 'showGroups',
+			'about': 'showAbout'
+		},
+		showMain: function() {
+			$('article').html('main');
+		},
+		showObjective: function() {
+			$('article').html('objective');
+		},
+		showActivity: function() {
+			$('article').html('activity');
+		},
+		showGroups: function() {
+			$('article').html('groups');
+		},
+		showAbout: function() {
+			$('article').html('about');
+		}
+	});
+	var router = new Router();
+	Backbone.history.start();
+	//Backbone.history.start({pushState: true});
+});
